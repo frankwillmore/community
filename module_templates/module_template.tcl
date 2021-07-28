42732b7209c44779cce4ea8c9c261e2b0bcf0eb3
Frank Willmore <willmore@anl.gov>
2021-07-28 20:10:00 +0000

    roughly complete project creation script and templete. need to test

--- module_templates/module_template.tcl
+++ module_templates/module_template.tcl
@@ -9,27 +9,31 @@ module-whatis "The [[PROJECT_NAME]] software project."
 
 proc ModulesHelp { } {
 
-puts stderr "- Project description: [PROJECT_DESCRIPTION]]"
+puts stderr "- Project description: [[PROJECT_DESCRIPTION]]"
 puts stderr "- This software is provided by the [[PROJECT_NAME]] project."
 puts stderr "- Project contact info: [[PROJECT_CONTACT]]"
 puts stderr "- Links to additional documentation: (e.g. github, bitbucket, or gitlab account, sphinx, etc.)"
 
 }
 
-set project_name_PREFIX /projects/project_name/opt/project_name
+set PROJECT_ROOT [[PROJECT_ROOT]]
 
-set project_name_PATH ${project_name_PREFIX}/bin
+#set project_name_PREFIX /projects/project_name/opt/project_name
+
+#set project_name_PATH ${project_name_PREFIX}/bin
+#prepend-path PATH $project_name_PATH
+set project_name_PATH ${PROJECT_ROOT}/bin
 prepend-path PATH $project_name_PATH
  
-set project_name_MANPATH ${project_name_PREFIX}/share/man
+set project_name_MANPATH ${PROJECT_ROOT}/share/man
 prepend-path MANPATH $project_name_PATH
  
-set project_name_PKG_CONFIG_PATH ${project_name_PREFIX}/lib64/pkgconfig
+set project_name_PKG_CONFIG_PATH ${PROJECT_ROOT}/lib64/pkgconfig
 prepend-path PKG_CONFIG_PATH $project_name_PKG_CONFIG_PATH
  
-set project_name_CMAKE_PREFIX_PATH ${project_name_PREFIX}/
+set project_name_CMAKE_PREFIX_PATH ${PROJECT_ROOT}
 prepend-path CMAKE_PREFIX_PATH $project_name_CMAKE_PREFIX_PATH
  
-set project_name_LD_LIBRARY_PATH ${project_name_PREFIX}/lib64
+set project_name_LD_LIBRARY_PATH ${PROJECT_ROOT}/lib64
 prepend-path LD_LIBRARY_PATH $project_name_LD_LIBRARY_PATH
  
